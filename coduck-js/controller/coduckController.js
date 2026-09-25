18330f6e0041e08837028c31b16f3ee343566c37
syphee <jameslandicho.21@ichat.sp.edu.sg>
2026-09-26 02:06:32 +0800

    fix: Fix ECONREFUSED for vercel deployment

--- coduck-js/controller/coduckController.js
+++ coduck-js/controller/coduckController.js
@@ -1,9 +1,15 @@
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import * as path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
 // Normal Message Query
 const fetchQuery = async (query) => {
   console.log(`[QUERY]:${query}\n`);
-
-  const response = await fetch("http://localhost:3000/api/chat", {
+  const API_URL = process.env.VERCEL_URL || "http://localhost:3000";
+  const response = await fetch(`${API_URL}/api/chat`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
