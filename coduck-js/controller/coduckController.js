577087913a60248c7ae1e99e45dd74ad784d2d66
syphee <jameslandicho.21@ichat.sp.edu.sg>
2026-09-26 02:36:22 +0800

    fix: fix extra slash on fetchQuery causing vercel error

--- coduck-js/controller/coduckController.js
+++ coduck-js/controller/coduckController.js
@@ -9,7 +9,7 @@ dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 const fetchQuery = async (query) => {
   console.log(`[QUERY]:${query}\n`);
   const API_URL = process.env.CODUCK_API_URL || "http://localhost:3000";
-  const response = await fetch(`${API_URL}/api/chat`, {
+  const response = await fetch(`${API_URL}api/chat`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
